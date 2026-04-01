4e18b85949cf36c20eb7af89e96972c2413df83a
Arslan Gurbanov <arslangeldi.gurbanov@gmail.com>
2026-04-02 01:10:35 +0800

    Create alpaca_to_chat_format.py

---
+++ data_pipeline/alpaca_to_chat_format.py
@@ -0,0 +1,88 @@
+import json, torch, re
+from typing import List
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+model_name = "microsoft/phi-3.5-mini-instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+
+paths = [
+    "your_path_here/ready_cat_1.jsonl",
+    "your_path_here/ready_cat_2.jsonl",
+    "your_path_here/ready_cat_3.jsonl",
+    "your_path_here/ready_cat_4.jsonl",
+    "your_path_here/ready_cat_5.jsonl",
+    "your_path_here/ready_cat_6.jsonl",
+    "your_path_here/ready_cat_7.jsonl"
+]
+
+output_path = "your_output_path/all_dialogues.jsonl"
+
+
+def write_to_jsonl(output_path, dialog, indx):
+    with open(output_path, mode="a", encoding="utf-8") as file:
+        packed_dialog = {f"dialog_{indx}": dialog}
+        x = json.dumps(packed_dialog) + "\n"
+        file.write(x)
+        file.flush()
+
+
+def read_all(paths: List[str], key: str = "deepseek_answer"):
+    # dialogs_list = list()
+    for i in paths:
+        with open(i, mode="r", encoding="utf-8") as file:
+            for obj in file:
+                try:
+                    json_l = json.loads(obj)
+                    deepseek_answer = json_l[key]
+                    yield deepseek_answer
+                except Exception:
+                    continue
+
+
+# -----------------------------------------------------------------------------------------------
+def split_and_filtr(dialog):
+    filterd = list()
+    splited = re.findall(r"(### Human:.*?### Jarvis:.*?(?=\s*### Human:|$))", dialog, flags=re.DOTALL)
+    for part in splited:
+        filterd.append(part)
+    return filterd
+
+
+def extract_all_after_marker(part):
+    patern3 = r"### (Human|Jarvis):([\s\S]*?)(?=\n### |$)"
+    match = re.finditer(patern3, part, flags=re.DOTALL)
+    if match:
+        return match
+    return None
+
+
+def prepare_extracted_replica(dialog):
+    for part in split_and_filtr(dialog):
+        for replica in extract_all_after_marker(part):
+            role = replica.group(1).strip()
+            content = replica.group(2).strip()
+            yield (role, content)
+
+
+def prepare_message(dialog):
+    splited_to_replicas = iter(prepare_extracted_replica(dialog))
+    message = list()
+    for role, content in splited_to_replicas:
+        if "Human" in role:
+            message.append({"role": "user", "content": content})
+        elif "Jarvis" in role:
+            message.append({"role": "assistant", "content": content})
+    return message
+
+
+def apply_chat_temp(dialog):
+    message_list = prepare_message(dialog)
+    x = tokenizer.apply_chat_template(message_list, tokenize=False, add_generation_prompt=False)
+    return x
+
+
+if __name__ == "__main__":
+    read_list = read_all(paths)
+    for n, dialog in enumerate(read_list):
+        result = apply_chat_temp(dialog)
+        write_to_jsonl(output_path, result, n)
