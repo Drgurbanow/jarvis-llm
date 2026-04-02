d0c7a4a7ecd3517a29fdc2151f03f5f405dea4ff
Arslan Gurbanov <arslangeldi.gurbanov@gmail.com>
2026-04-02 18:05:11 +0800

    Update rag_inference.py

--- rag_pipline/rag_inference.py
+++ rag_pipline/rag_inference.py
@@ -3,23 +3,28 @@ RAG (Retrieval-Augmented Generation) pipeline for JARVIS assistant.
 Uses FAISS vector store and BAAI/bge-base-en-v1.5 embeddings.
 """
 
-import faiss
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, Settings
+import faiss, torch, os
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, Settings, load_index_from_storage
 from llama_index.vector_stores.faiss import FaissVectorStore
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.readers.file import PyMuPDFReader
 
-Settings.llm = None #Turn off openAI llm, cause i oonly need answer
+BASE_DIR = os.path.dirname(__file__)
+EMBEDDING_MODEL_PATH = os.path.join(BASE_DIR, "models/bge-base-en-v1.5")
+DOCUMENTS_PATH = [os.path.join(BASE_DIR, "data/knowledge_base.txt")]
+
+Settings.llm = None #Turn off openAI llm, cause i only need answer
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 Settings.embed_model = HuggingFaceEmbedding(
-    model_name=r"C:\Users\user\.cache\huggingface\hub\models--BAAI--bge-base-en-v1.5",
+    model_name=EMBEDDING_MODEL_PATH,
     query_instruction="Represent this sentence for searching relevant passages: ",
-    device="cuda",
+    device=device,
 )
 
 documents = SimpleDirectoryReader(
-    input_files=[r"C:\Users\user\Desktop\Rag_test.txt"],
+    input_files=DOCUMENTS_PATH,
     file_extractor={".pdf": PyMuPDFReader()} # others auto detect
 ).load_data()
 
@@ -32,17 +37,19 @@ faiss_index.hnsw.efConstruction = 200
 vector_store = FaissVectorStore(faiss_index=faiss_index)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-index = VectorStoreIndex.from_documents(documents, storage_context=storage_context,
-                                        transformations=[parser], show_progress=True
+index = VectorStoreIndex.from_documents(documents, 
+                                        storage_context=storage_context,
+                                        transformations=[parser], 
+                                        show_progress=True
                                         )
 
 #index.storage_context.persist(persist_dir="./my_rag")
 
-"""query_engine = index.as_query_engine(similarity_top_k=3)
-response = query_engine.query("Jarvis, run a diagnostic on my sleep patterns for the last month. And don't sugarcoat it.")
-"""
+
 retriever_engine = index.as_retriever(similarity_top_k=3)
 response = retriever_engine.retrieve("Jarvis, can you tell who is Francesco Cirillo, and what he created?")
+sorted_nodes = sorted(response, key=lambda x: x[0])
+selected_nodes = sorted_nodes[:2]
 
 print("\nОтвет:")
 
